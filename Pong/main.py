aa23f28c6fa15bb7c3b6f14c15cd71fc356b9560
Alireza Razinejad <a.razinejad@gmail.com>
2021-07-02 11:56:28 +0430

    Let's play pong with sound

--- Pong/main.py
+++ Pong/main.py
@@ -1,4 +1,5 @@
 import turtle
+import winsound
 
 # Game main
 
@@ -97,13 +98,16 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
+    # Border checking
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound('border.wav', winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound('border.wav', winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -111,6 +115,7 @@ while True:
         score_a += 1
         pen.clear()
         pen.write(f'Player A: {score_a}  Player B: {score_b}', align='center', font=('Courier', 24, 'normal'))
+        winsound.PlaySound('score.wav', winsound.SND_ASYNC)
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
@@ -118,13 +123,16 @@ while True:
         score_b += 1
         pen.clear()
         pen.write(f'Player A: {score_a}  Player B: {score_b}', align='center', font=('Courier', 24, 'normal'))
+        winsound.PlaySound('score.wav', winsound.SND_ASYNC)
 
     # Paddle and Ball collision
 
     if (350 > ball.xcor() > 340) and ((paddle_b.ycor() + 50) > ball.ycor() > (paddle_b.ycor() - 50)):
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound('paddle.wav', winsound.SND_ASYNC)
 
     if (-350 < ball.xcor() < -340) and ((paddle_a.ycor() + 50) > ball.ycor() > (paddle_a.ycor() - 50)):
         ball.setx(-340)
         ball.dx *= -1
+        winsound.PlaySound('paddle.wav', winsound.SND_ASYNC)
