29d8d04dad5eb8c4d14a3efc5445d13a68a637f2
Alireza Razinejad <a.razinejad@gmail.com>
2021-07-07 15:44:39 +0430

    Initialized snake game with logic skeleton

---
+++ Snake/main.py
@@ -0,0 +1,72 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+class Cube(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class Snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def add_cube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def draw_grid(w, rows, surface):
+    pass
+
+
+def redraw_window(surface):
+    pass
+
+
+def random_snack(rows, items):
+    pass
+
+
+def message_box(subject, content):
+    pass
+
+
+def main():
+    width = 500
+    height = 500
+    rows = 20
+    win = pygame.display.set_mode((width, height))
+    s = Snake((255, 0, 0), (10, 10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redraw_window(win)
+    pass
+
+
+main()
